f3b95eeac1ce101c7b149fcf3c39397b3eb1bfa6
Davy <945825394@qq.com>
2022-08-05 00:25:23 +0800

    While coding, a problem occured 'resualt pointer != nulptr' don't know why just yet, but I'm going to figure it out tomorrow.

--- Double Float Culculation/Double Float Culculation/Main.c
+++ Double Float Culculation/Double Float Culculation/Main.c
@@ -4,20 +4,30 @@
 int main()
 {
 	int tmp1 = Menu();
-	switch (tmp1) :
+	switch (tmp1)
 	{
 	case 1:
-		double arr[1000] = { 0 };
+		double arr[20] = { 0 };
 		int i = 0;
-		while(i<=1000)
+		while (i <= 1000)
 		{
-			scanf_s("%lf",arr[i], 1);
-			double tmp2 = Add(arr,);
+			scanf_s("%lf", arr[i], 1);
+			if (arr[i] == -1)
+				break;
+			i++;
 		}
-
+		int tmp = 0;
+		for (int tmp = 0; arr[tmp] != 0; tmp++)
+		{
+			printf("%lf ", arr[tmp]);
+		}
+		break;
 	case 2:
+		break;
 	case 3:
+		break;
 	case 4:
+		break;
 	}
 	return 0;
 }
