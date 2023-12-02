90f28ad58c4ef9932dac8e29ef6c4773d4a9f951
Davy <945825394@qq.com>
2023-12-02 22:34:28 +0800

    Wrote a c for resetting IE settings for VPN uses

---
+++ Reset IE Settings/Reset IE Settings/Reset IE Settings.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    int result = system("RunDll32.exe InetCpl.cpl,ResetIEtoDefaults");
+    if (result == 0) {
+        printf("Internet Explorer settings reset successfully.\n");
+    }
+    else {
+        printf("Failed to reset Internet Explorer settings.\n");
+    }
+    return result;
+}
